9ea33fdf44766b1e1d06b2b2a6073a7346ee01d1
Godfadatun <danyadegoke@gmail.com>
2020-08-25 03:28:43 -0400

    pg db

--- config/database.js
+++ config/database.js
@@ -4,15 +4,25 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
+        client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'branchless_cms'),
-        username: env('DATABASE_USERNAME', 'root'),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false),
+        // client: 'mysql',
+        // host: env('DATABASE_HOST', '127.0.0.1'),
+        // port: env.int('DATABASE_PORT', 3306),
+        // database: env('DATABASE_NAME', 'branchless_cms'),
+        // username: env('DATABASE_USERNAME', 'root'),
+        // password: env('DATABASE_PASSWORD', ''),
+        // ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {}
+      // options: {}
+      options: {
+        autoMigration: true,
+        ssl: false,
+      },
     },
   },
 });
